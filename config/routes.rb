899f98dd89a4f65f2b0e6403424c5949504856fc
Drew Krehel <drewkrehel@gmail.com>
2025-09-08 17:50:23 +0000

    still trying to get the first route to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
-  root "calc#index"
+  
+  # root "calc#index"
   get "/square", to: "calc#square"
+  get("/", { :controller => "calc", :action => "square" })
+  
 end
